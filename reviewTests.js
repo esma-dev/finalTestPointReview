2ef5d5f64a83f64b2bead71906052c61b5dfb7bd
Asmaa H Abbas <abbas.asmaa@gmail.com>
2017-07-04 01:40:49 -0400

    added problems from 05-Functions-&-Closures

--- reviewTests.js
+++ reviewTests.js
@@ -1,3 +1,5 @@
+//05 - Functions & Closure (functions.js)
+
 /*// Given a function, return a new function will only run once, no matter how many times it's called
 describe('once', function() {
   it('should only increment num one time', function() {
@@ -29,3 +31,76 @@ function once(func){
       }
     };
 };
+
+/******************************************************************************/
+//05 - Functions & Closure (functions.js)
+
+/*
+#### Guest List Functions
+
+The `guestListFns` function accepts two arguments:
+
+- Guest List Array: This is the original guest list your superior gave you
+- Secret Code: The secret code can be any JavaScript value
+
+The `guestListFns` returns an array of functions.  Each function in the array
+returned accepts a `Secret Code` as an argument.  When one of the functions in
+the array is invoked with the proper secret code (the same code passed
+to the `guestListFns`), it will return one of the guest's names in the original
+Guest List Array.  The values in the functions can't be changed, so the guest names
+can't be changed or manipulated from a corrupt individual! 
+
+
+
+Example:
+
+```javascript
+
+var guestListFns = guestListFns(["Gabriel", "Ben", "Dan", "Griffin", "Cang", "Kate", "Chris"], 512);
+
+var guest = guestListFns[1](512); // guest ==> "Guest-Ben";
+
+*/
+
+//Solution01
+function guestListFns(gstListArr, code){
+  var allGsts = [];
+
+  for(var i = 0; i < gstListArr.length; i++){
+    allGsts.push(function(j){
+      return function(secretCode){
+        if(secretCode === code){
+          return gstListArr[j];
+        } else {
+          return 'Secret-Code: Invalid';
+        }
+      }
+    }(i));
+  }
+
+  return allGsts;
+}
+
+
+//Solution02
+function guestListFns(guests, secret){
+  var result = [];
+  var guest;
+
+  var wrapper = function(secret, guest){
+    return function(secretNum){
+      if(secretNum === secret){
+        return guest;
+      } else {
+        return 'Secret-Code: Invalid';
+      }
+    }
+  };
+
+  for(var i = 0; i < guests.length; i++){
+    guest = guests[i];
+    result.push(wrapper(secret, guest));
+  };
+
+  return result;
+}
